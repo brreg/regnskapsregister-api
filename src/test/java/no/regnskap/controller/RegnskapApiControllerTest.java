a1c52ebce6f3b223cd621f4981d64df8507c3739
NilsOveTen <nils.ove.tendenes@acando.no>
2019-05-20 11:50:05 +0200

    rm generated pom

--- src/test/java/no/regnskap/controller/RegnskapApiControllerTest.java
+++ src/test/java/no/regnskap/controller/RegnskapApiControllerTest.java
@@ -1,16 +1,12 @@
 package no.regnskap.controller;
 
-import no.regnskap.generated.model.Regnskap;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.List;
 
 
 @RunWith(SpringRunner.class)
@@ -23,12 +19,5 @@ public class RegnskapApiControllerTest {
     public void happyDay()
     {
         Assert.assertTrue(true);
-    }
-
-    @Test
-    public void getRegnskapTest() {
-        RegnskapApiControllerImpl invoicesApiController = new RegnskapApiControllerImpl();
-        ResponseEntity<List<Regnskap>> response = invoicesApiController.getRegnskap(httpServletRequestMock, "123");
-        Assert.assertNotEquals(HttpStatus.NOT_IMPLEMENTED, response.getStatusCode());
     }
 }
