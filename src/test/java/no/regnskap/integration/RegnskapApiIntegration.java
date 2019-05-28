93442647f6db85539ccdffdf9f07a6331c72d156
NilsOveTen <nils.ove.tendenes@acando.no>
2019-05-28 14:03:59 +0200

    regnskapRootURL -> URL.class

--- src/test/java/no/regnskap/integration/RegnskapApiIntegration.java
+++ src/test/java/no/regnskap/integration/RegnskapApiIntegration.java
@@ -16,6 +16,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 
 import static no.regnskap.TestData.*;
@@ -24,7 +25,7 @@ import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
 
 @RunWith(MockitoJUnitRunner.class)
 public class RegnskapApiIntegration {
-    private static String regnskapRootURL;
+    private static URL regnskapRootURL;
 
     @ClassRule
     public static DockerComposeContainer compose = new DockerComposeContainer<>(new File("src/test/resources/test-compose.yml"))
@@ -33,7 +34,7 @@ public class RegnskapApiIntegration {
         .withPull(false);
 
     @BeforeClass
-    public static void mongoSetup() {
+    public static void mongoSetup() throws MalformedURLException {
         CodecRegistry pojoCodecRegistry = fromRegistries(MongoClient.getDefaultCodecRegistry(), fromProviders(PojoCodecProvider.builder().automatic(true).build()));
         MongoClient mongoClient = new MongoClient(compose.getServiceHost(MONGO_SERVICE_NAME, MONGO_PORT), compose.getServicePort(MONGO_SERVICE_NAME, MONGO_PORT));
         MongoDatabase mongoDatabase = mongoClient.getDatabase(DATABASE_NAME).withCodecRegistry(pojoCodecRegistry);
@@ -42,32 +43,31 @@ public class RegnskapApiIntegration {
         mongoCollection.insertOne(regnskap2017);
         mongoCollection.insertOne(regnskap2018);
 
-        regnskapRootURL = "http://" + compose.getServiceHost(API_SERVICE_NAME, API_PORT) + ":" + compose.getServicePort(API_SERVICE_NAME, API_PORT);
+        regnskapRootURL = new URL("http://" + compose.getServiceHost(API_SERVICE_NAME, API_PORT) + ":" + compose.getServicePort(API_SERVICE_NAME, API_PORT));
     }
 
     @Test
     public void pingTest() throws Exception {
-        String response = simpleGet(regnskapRootURL + "/ping");
+        String response = simpleGet(new URL(regnskapRootURL + "/ping"));
         Assert.assertEquals("RegnskapAPI is available", "pong", response);
     }
 
     @Test
     public void getByOrgnrTest() throws Exception {
-        String response = simpleGet(regnskapRootURL + "/regnskap?orgNummer=orgnummer");
+        String response = simpleGet(new URL(regnskapRootURL + "/regnskap?orgNummer=orgnummer"));
         Assert.assertEquals(EXPECTED_RESPONSE_ORGNR, response);
     }
 
     @Test
     public void getById() throws Exception {
-        String response2018 = simpleGet(regnskapRootURL + "/regnskaps/" + GENERATED_ID_0.toHexString());
-        String response2017 = simpleGet(regnskapRootURL + "/regnskaps/" + GENERATED_ID_1.toHexString());
+        String response2018 = simpleGet(new URL(regnskapRootURL + "/regnskaps/" + GENERATED_ID_0.toHexString()));
+        String response2017 = simpleGet(new URL(regnskapRootURL + "/regnskaps/" + GENERATED_ID_1.toHexString()));
         Assert.assertEquals(buildExpectedDatabaseResponse(GENERATED_ID_0, 2018), response2018);
         Assert.assertEquals(buildExpectedDatabaseResponse(GENERATED_ID_1, 2017), response2017);
     }
 
-    private String simpleGet(String address) throws Exception {
-        URL obj = new URL(address);
-        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+    private String simpleGet(URL address) throws Exception {
+        HttpURLConnection con = (HttpURLConnection) address.openConnection();
         con.setRequestMethod("GET");
 
         BufferedReader reader = new BufferedReader(new InputStreamReader(con.getInputStream()));
