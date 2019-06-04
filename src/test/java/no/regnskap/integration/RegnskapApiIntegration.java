8a09d28286f041a1258fb2bd49b2bb7625d0e95f
NilsOveTen <nils.ove.tendenes@acando.no>
2019-06-04 10:37:26 +0200

    Only integration ping test

--- src/test/java/no/regnskap/integration/RegnskapApiIntegration.java
+++ src/test/java/no/regnskap/integration/RegnskapApiIntegration.java
@@ -9,7 +9,11 @@ import org.bson.codecs.pojo.PojoCodecProvider;
 import org.junit.*;
 import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.test.context.TestPropertySource;
 import org.testcontainers.containers.DockerComposeContainer;
+import org.testcontainers.containers.output.Slf4jLogConsumer;
 import org.testcontainers.containers.wait.strategy.Wait;
 
 import java.io.BufferedReader;
@@ -22,31 +26,36 @@ import java.net.URL;
 import static no.regnskap.TestData.*;
 import static org.bson.codecs.configuration.CodecRegistries.fromProviders;
 import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
-/*
+
 @RunWith(MockitoJUnitRunner.class)
 @Ignore //ignore service test until it is working on Jenkins
+@TestPropertySource(locations="classpath:test.properties")
 public class RegnskapApiIntegration {
+    private static ClassLoader classLoader = RegnskapApiIntegration.class.getClassLoader();
+    private final static Logger logger = LoggerFactory.getLogger(RegnskapApiIntegration.class);
+    private static Slf4jLogConsumer mongoLog = new Slf4jLogConsumer(logger).withPrefix("mongo-container");
+    private static Slf4jLogConsumer apiLog = new Slf4jLogConsumer(logger).withPrefix("api-container");
 
     @ClassRule
-    public static DockerComposeContainer compose = new DockerComposeContainer<>(new File("src/test/resources/test-compose.yml"))
+    public static DockerComposeContainer compose = new DockerComposeContainer<>(new File(classLoader.getResource("test-compose.yml").getFile()))
         .withExposedService(MONGO_SERVICE_NAME, MONGO_PORT, Wait.forListeningPort())
         .withExposedService(API_SERVICE_NAME, API_PORT, Wait.forHttp("/ping").forStatusCode(200))
-        .withPull(false);
+        .withPull(false)
+        .withLogConsumer(MONGO_SERVICE_NAME, mongoLog)
+        .withLogConsumer(API_SERVICE_NAME, apiLog);
 
     @BeforeClass
     public static void mongoSetup() {
-        CodecRegistry pojoCodecRegistry = fromRegistries(MongoClient.getDefaultCodecRegistry(), fromProviders(PojoCodecProvider.builder().automatic(true).build()));
-        //MongoCredential credentials = MongoCredential.createScramSha1Credential(MONGO_USER, DATABASE_NAME, MONGO_PASSWORD);
-        //ServerAddress serverAddress = new ServerAddress(compose.getServiceHost(MONGO_SERVICE_NAME, MONGO_PORT), compose.getServicePort(MONGO_SERVICE_NAME, MONGO_PORT));
-        //List<ServerAddress> addressList = Collections.singletonList(serverAddress);
-        //MongoClientOptions options = MongoClientOptions.builder().build();
-        MongoClientURI uri = new MongoClientURI(MONGO_URI);
-        MongoClient mongoClient = new MongoClient(uri);
+        /*CodecRegistry pojoCodecRegistry = fromRegistries(MongoClient.getDefaultCodecRegistry(), fromProviders(PojoCodecProvider.builder().automatic(true).build()));
+        ServerAddress serverAddress = new ServerAddress(compose.getServiceHost(API_SERVICE_NAME, API_PORT), compose.getServicePort(API_SERVICE_NAME, API_PORT));
+        MongoCredential credentials = MongoCredential.createScramSha1Credential(MONGO_USER, DATABASE_NAME, MONGO_PASSWORD);
+        MongoClientOptions options = MongoClientOptions.builder().build();
+        MongoClient mongoClient = new MongoClient(serverAddress, credentials, options);
         MongoDatabase mongoDatabase = mongoClient.getDatabase(DATABASE_NAME).withCodecRegistry(pojoCodecRegistry);
         MongoCollection<RegnskapDB> mongoCollection = mongoDatabase.getCollection(COLLECTION_NAME).withDocumentClass(RegnskapDB.class);
 
         mongoCollection.insertOne(regnskap2017);
-        mongoCollection.insertOne(regnskap2018);
+        mongoCollection.insertOne(regnskap2018);*/
     }
 
     @Test
@@ -54,7 +63,7 @@ public class RegnskapApiIntegration {
         String response = simpleGet(buildRegnskapURL("/ping"));
         Assert.assertEquals("RegnskapAPI is available", "pong", response);
     }
-
+/*
     @Test
     public void getByOrgnrTest() throws Exception {
         String response = simpleGet(buildRegnskapURL("/regnskap?orgNummer=orgnummer"));
@@ -68,7 +77,7 @@ public class RegnskapApiIntegration {
         Assert.assertEquals(buildExpectedDatabaseResponse(GENERATED_ID_0, 2018), response2018);
         Assert.assertEquals(buildExpectedDatabaseResponse(GENERATED_ID_1, 2017), response2017);
     }
-
+*/
     private String simpleGet(URL address) throws Exception {
         HttpURLConnection con = (HttpURLConnection) address.openConnection();
         con.setRequestMethod("GET");
@@ -84,4 +93,4 @@ public class RegnskapApiIntegration {
     private URL buildRegnskapURL(String address) throws MalformedURLException {
         return new URL("http", compose.getServiceHost(API_SERVICE_NAME, API_PORT), compose.getServicePort(API_SERVICE_NAME, API_PORT), address);
     }
-}*/
+}
