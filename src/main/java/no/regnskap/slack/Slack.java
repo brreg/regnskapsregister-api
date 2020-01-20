856ec873bfbadf0b5d7c483c9f1f8bc252572d6b
Frode Roxrud Gill <extfrg@brreg.no>
2020-01-20 13:25:14 +0100

    Some debug logging

--- src/main/java/no/regnskap/slack/Slack.java
+++ src/main/java/no/regnskap/slack/Slack.java
@@ -4,14 +4,18 @@ import com.hubspot.algebra.Result;
 import com.hubspot.slack.client.methods.params.chat.ChatPostMessageParams;
 import com.hubspot.slack.client.models.response.SlackError;
 import com.hubspot.slack.client.models.response.chat.ChatPostMessageResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 public class Slack {
+    private static Logger LOGGER = LoggerFactory.getLogger(Slack.class);
 
     public static final String PRODFEIL_CHANNEL = "#prodfeil";
 
 
     public static ChatPostMessageResponse postMessage(final String token, final String channel, final String message) {
+        LOGGER.info("token = (" + token.length() + "), " + token.substring(0, 5));
         if (token==null || token.isEmpty()) {
             return null;
         }
