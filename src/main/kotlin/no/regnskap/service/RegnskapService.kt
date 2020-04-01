6072b887254fc4196936568cc3794eb12509639b
Frode Roxrud Gill <extfrg@brreg.no>
2020-04-01 11:11:51 +0200

    Actually filter on year

--- src/main/kotlin/no/regnskap/service/RegnskapService.kt
+++ src/main/kotlin/no/regnskap/service/RegnskapService.kt
@@ -22,12 +22,21 @@ class RegnskapService (
         regnskapRepository
             .findByIdOrNull(id)
             ?.mapPersistenceToGenerated()
+            ?.let {regnskap ->
+                if (year == null ||
+                    (regnskap.regnskapsperiode.fraDato.year<=year && regnskap.regnskapsperiode.tilDato.year>=year)) regnskap
+                else null
+            }
 
     fun getByOrgnr(orgnr: String, year: Int?): List<Regnskap> =
         regnskapRepository
             .findByOrgnrOrderByJournalnrDesc(orgnr) // Sort by journalnr, highest value is the most recent data
             .filter { it.aarsregnskapstype.toLowerCase() == Regnskap.OppstillingsplanEnum.STORE.value } // "Husk også at det er ikke alle regnskapene som ligger i masse-filene som skal være tilgjengelig, kun de som følger "store" oppstillingsplaner"
             .filter { it.regnskapstype.toLowerCase() == REGNSKAPSTYPE_SELSKAP } // "Men for konsernselskap, så leverer den regnskapet for konsernet, og ikke for selskapet. Her må det endres slik at det er regnskapet for selskapet som leveres. Dette fremkommer av feltet "regnskapstype". Her har du verdiene S og K, der K står for konsern. Vi skal kun vise dem som har regnskapstype S, og ikke K."
+            ?.filter {regnskap ->
+                if (year == null) true
+                else regnskap.regnaar == year
+            }
             .distinctBy { it.regnaar } // Filters the list by the first objects with a distinct year, since it's already sorted the list will be the most recent data for each year
             .maxBy { it.regnaar } // Only return data from the last registered year
             .let {
