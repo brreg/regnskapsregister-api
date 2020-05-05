151480c0fd15a5a9eda797f263e8addffe509de2
Frode Roxrud Gill <extfrg@brreg.no>
2020-05-05 15:47:55 +0200

    Added fraDato and tilDato for all statistic endpoints

--- src/main/kotlin/no/regnskap/controller/StatistikkApiImpl.kt
+++ src/main/kotlin/no/regnskap/controller/StatistikkApiImpl.kt
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
+import java.time.LocalDate
 
 import javax.servlet.http.HttpServletRequest
 
@@ -15,15 +16,15 @@ open class StatistikkApiImpl(
         private val restcallLogService: RestcallLogService
 ): no.regnskap.generated.api.StatistikkApi {
 
-    override fun getStatisticsByIp(httpServletRequest: HttpServletRequest): ResponseEntity<List<String>> {
+    override fun getStatisticsByIp(httpServletRequest: HttpServletRequest, fraDato: LocalDate?, tilDato: LocalDate?): ResponseEntity<List<String>> {
         return ResponseEntity(HttpStatus.NOT_IMPLEMENTED);
     }
 
-    override fun getStatisticsByMethod(httpServletRequest: HttpServletRequest): ResponseEntity<List<String>> {
+    override fun getStatisticsByMethod(httpServletRequest: HttpServletRequest, fraDato: LocalDate?, tilDato: LocalDate?): ResponseEntity<List<String>> {
         return ResponseEntity(HttpStatus.NOT_IMPLEMENTED);
     }
 
-    override fun getStatisticsByOrgnr(httpServletRequest: HttpServletRequest): ResponseEntity<List<String>> {
+    override fun getStatisticsByOrgnr(httpServletRequest: HttpServletRequest, fraDato: LocalDate?, tilDato: LocalDate?): ResponseEntity<List<String>> {
         return ResponseEntity(HttpStatus.NOT_IMPLEMENTED);
     }
 
