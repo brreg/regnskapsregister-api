514ad2d178e2da184745c639d0df0db057d25c4a
Bjørn Grøva <bjg@brreg.no>
2021-04-15 15:27:18 +0200

    DBF-847: Catch exception in servlet
    Recommentdation from Sonar

--- src/main/java/no/brreg/regnskap/spring/CachableDispatcherServlet.java
+++ src/main/java/no/brreg/regnskap/spring/CachableDispatcherServlet.java
@@ -6,15 +6,23 @@ import org.springframework.web.util.ContentCachingRequestWrapper;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class CachableDispatcherServlet extends DispatcherServlet {
+    private static final Logger LOGGER = LoggerFactory.getLogger(CachableDispatcherServlet.class);
 
     @Override
     protected void doDispatch(HttpServletRequest request, HttpServletResponse response) throws Exception {
         if (!(request instanceof ContentCachingRequestWrapper)) {
             request = new ContentCachingRequestWrapper(request);
         }
-        super.doDispatch(request, response);
+        try {
+            super.doDispatch(request, response);
+        } catch (Exception e) {
+            LOGGER.error("Dispatch failed: ", e);
+        }
+        
     }
 
 }
