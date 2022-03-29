e262faa09704618d2888a641a32e619272dbf36a
olw <olw@brreg.no>
2022-03-29 17:23:37 +0200

    * Removed ST from servers * Version 1.3.0 * Fixed duplicate log in swagger by changing tags

--- src/main/java/no/brreg/regnskap/spring/ApplicationInfo.java
+++ src/main/java/no/brreg/regnskap/spring/ApplicationInfo.java
@@ -1,5 +1,5 @@
 package no.brreg.regnskap.generated.spring;
 
 public class ApplicationInfo {
-    public static final String VERSION = "1.1.12";
+    public static final String VERSION = "1.3.0";
 }
