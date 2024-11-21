16d0a33f35f834bca5a6953fd67fdfc9de239181
Michael Bråten <mib@brreg.no>
2024-11-21 08:49:40 +0100

    Fix loading of mock file

--- src/main/java/no/brreg/regnskap/service/AarsregnskapCopyService.java
+++ src/main/java/no/brreg/regnskap/service/AarsregnskapCopyService.java
@@ -1,9 +1,12 @@
 package no.brreg.regnskap.service;
 
+import no.brreg.regnskap.controller.exception.InternalServerError;
 import no.brreg.regnskap.repository.AarsregnskapRepository;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
 import java.time.Clock;
 import java.time.Year;
 import java.util.List;
@@ -16,13 +19,14 @@ public class AarsregnskapCopyService {
     private final AarsregnskapRepository aarsregnskapRepository;
     private final Clock clock;
 
-    @Value("${TEST_FILE_PATH:./src/test/resources/aarsregnskap-files/example-multipage.tiff}")
-    private String testFilePath;
+    @Value("classpath:aarsregnskap-mock-files/example-multipage.tiff")
+    private Resource mockFileResource;
 
     public AarsregnskapCopyService(PdfConverterService pdfConverterService, AarsregnskapRepository aarsregnskapRepository, Clock clock) {
         this.pdfConverterService = pdfConverterService;
         this.aarsregnskapRepository = aarsregnskapRepository;
         this.clock = clock;
+
     }
 
     public List<String> getAvailableAarsregnskapYears(String orgnr) {
@@ -40,6 +44,13 @@ public class AarsregnskapCopyService {
             return Optional.empty();
         }
 
-        return Optional.of(pdfConverterService.tiffToPdf(testFilePath));
+        String mockFilePath;
+        try {
+            mockFilePath = mockFileResource.getURI().getPath();
+        } catch (IOException e) {
+            throw new InternalServerError(e);
+        }
+
+        return Optional.of(pdfConverterService.tiffToPdf(mockFilePath));
     }
 }
