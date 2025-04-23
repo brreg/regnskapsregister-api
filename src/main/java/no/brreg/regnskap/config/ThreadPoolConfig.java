89ceae85576609e80fc5aae2a07c40f42f1fb04b
Michael Bråten <mib@brreg.no>
2025-04-23 14:39:40 +0200

    * New implementation of converter (#108)
    * Remove graphicsmagick

---
+++ src/main/java/no/brreg/regnskap/config/ThreadPoolConfig.java
@@ -0,0 +1,15 @@
+package no.brreg.regnskap.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@Configuration
+public class ThreadPoolConfig {
+    @Bean
+    public ExecutorService tashExecutor() {
+        return Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+    }
+}
